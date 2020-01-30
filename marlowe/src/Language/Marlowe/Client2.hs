9806534670cbf0d39b403b203261668702fb4757
Alexander Nemish <anemish@gmail.com>
2020-01-30 19:50:28 +0200

    wip

--- marlowe/src/Language/Marlowe/Client2.hs
+++ marlowe/src/Language/Marlowe/Client2.hs
@@ -20,6 +20,7 @@
 module Language.Marlowe.Client2 where
 import           Control.Monad              (Monad (..), void)
 import           Control.Monad.Error.Class  (MonadError (..))
+import           Control.Monad.Error.Lens   (throwing)
 import           Data.Map                   (Map)
 import qualified Data.Map                   as Map
 import           Data.Maybe                 (maybeToList)
@@ -27,7 +28,7 @@ import qualified Data.Set                   as Set
 import qualified Data.Text                  as Text
 import           Language.Plutus.Contract
 import qualified Language.Plutus.Contract.Tx  as Tx
-import           Language.Marlowe.Semantics (Input, MarloweData(..), emptyState, marloweValidator)
+import           Language.Marlowe.Semantics hiding (Contract)
 import qualified Language.Marlowe.Semantics as Marlowe
 import qualified Language.PlutusTx          as PlutusTx
 import qualified Language.PlutusTx.Prelude  as P
@@ -43,24 +44,33 @@ import qualified Ledger.Value               as Val
 type MarloweSchema =
     BlockchainActions
         .\/ Endpoint "create" Marlowe.Contract
+        .\/ Endpoint "apply-inputs" (Address, [Input])
 
 
 marloweContract2 :: forall e. (AsContractError e)
     => Contract MarloweSchema e ()
-marloweContract2 = do
-    cont <- endpoint @"create" @Marlowe.Contract @MarloweSchema
-    createContract cont
+marloweContract2 = create >> apply
+  where
+    create = do
+        cont <- endpoint @"create" @Marlowe.Contract @MarloweSchema
+        createContract cont
+    apply = do
+        (creator, inputs) <- endpoint @"apply-inputs" @(PubKeyHash, [Input]) @MarloweSchema
+
+
+
 
 {-| Create a Marlowe contract.
     Uses wallet public key to generate a unique script address.
  -}
 createContract :: (AsContractError e)
     => Marlowe.Contract
-    -> Contract MarloweSchema e ()
+    -> Contract MarloweSchema e PubKeyHash
 createContract contract = do
     slot <- awaitSlot 0
     creator <- pubKeyHash <$> ownPubKey
     let validator = validatorScript creator
+        address = Ledger.scriptAddress validator
 
         marloweData = MarloweData {
             marloweCreator = creator,
@@ -71,9 +81,86 @@ createContract contract = do
     let payValue = adaValueOf 1
 
     let slotRange = interval slot (slot + 10)
-    let tx = payToScript payValue (Ledger.scriptAddress validator) ds
+    let tx = payToScript payValue address ds
              <> mustBeValidIn slotRange
     void $ submitTx tx
+    pure creator
+
+
+applyInputs :: (AsContractError e)
+    => PubKeyHash
+    -> [Input]
+    -> Contract MarloweSchema e ()
+applyInputs creator inputs = do
+    let redeemer = mkRedeemer inputs
+        validator = validatorScript marloweCreator
+        dataValue = DataValue (PlutusTx.toData marloweData)
+        address = scriptAddress validator
+    slot <- awaitSlot 0
+    utxo <- utxoAt address
+
+
+    let find _ tx = case txOutTxData tx of
+            Just dv -> case PlutusTx.fromData dv of
+                Just marloweData ->
+    let tx = collectFromScriptFilter find utxo validator inputs
+    let asdf = foldl ()
+    let ourUtxo = Map.filterWithKey flt utxo
+        mkIn :: TxOutRef -> DataValue -> TxIn
+        mkIn ref = scriptTxIn ref vls red
+        inputs =
+            fmap (\(ref, dat, val) -> (mkIn ref dat, val)) $
+            mapMaybe (\(ref, out) -> (ref,,txOutValue $ txOutTxOut out) <$> txOutTxData out) $
+            Map.toList ourUtxo
+
+    -- For now, we expect a transaction to happen whithin 10 slots from now.
+    -- That's about 3 minutes, should be fine.
+    let slotRange = interval slot (slot + Slot 10)
+    let txInput = TransactionInput {
+            txInterval = (slot, slot + Slot 10),
+            txInputs = inputs }
+
+    let computedResult = computeTransaction txInput marloweState marloweContract
+
+    tx <- case computedResult of
+        TransactionOutput {txOutPayments, txOutState, txOutContract} -> do
+
+            let marloweData = MarloweData {
+                    marloweCreator,
+                    marloweContract = txOutContract,
+                    marloweState = txOutState }
+
+            let deducedTxOutputs = case txOutContract of
+                    Close -> txPaymentOuts txOutPayments
+                    _ -> let
+                        txWithPayouts = txPaymentOuts txOutPayments
+                        totalPayouts = foldMap (\(Payment _ v) -> v) txOutPayments
+                        finalBalance = totalIncome P.- totalPayouts
+                        dataValue = DataValue (PlutusTx.toData marloweData)
+                        scriptOut = payToScript finalBalance address dataValue
+                        in scriptOut <> txWithPayouts
+
+            return (deducedTxOutputs)
+        Error txError -> throwing _OtherError $ (Text.pack $ show txError)
+
+    void $ submitTx (tx <> mustBeValidIn slotRange)
+  where
+    collectDeposits (IDeposit _ _ (Token cur tok) amount) = Val.singleton cur tok amount
+    collectDeposits _                    = P.zero
+
+    totalIncome = foldMap collectDeposits inputs
+
+    txPaymentOuts :: [Payment] -> UnbalancedTx
+    txPaymentOuts payments = let
+        ps = foldr collectPayments Map.empty payments
+        in foldMap (\(pk, value) -> payToPubKeyHash value pk) (Map.toList ps)
+
+    collectPayments :: Payment -> Map Party Money -> Map Party Money
+    collectPayments (Payment party money) payments = let
+        newValue = case Map.lookup party payments of
+            Just value -> value P.+ money
+            Nothing    -> money
+        in Map.insert party newValue payments
 
 
 {-| Generate a validator script for 'creator' PubKey -}
--- marlowe/test/Spec/Marlowe/Marlowe2.hs
+++ marlowe/test/Spec/Marlowe/Marlowe2.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE ExplicitForAll    #-}
+{-# LANGUAGE NumericUnderscores  #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications  #-}
 {-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns
@@ -14,12 +15,13 @@ where
 import           Test.Tasty
 
 import           Language.Marlowe.Semantics
+import           Language.Marlowe.Util
 import           Language.Marlowe.Client2
 import           Language.Plutus.Contract hiding (Contract)
 import           Language.Plutus.Contract.Test
 import           Language.PlutusTx.Lattice
-import qualified Ledger
-import qualified Ledger.Ada                                            as Ada
+import           Ledger
+import           Ledger.Ada                 (adaValueOf)
 import           Ledger.Value                                          (TokenName, Value)
 
 
@@ -28,12 +30,51 @@ tests = testGroup "token account"
     [ checkPredicate @MarloweSchema @ContractError "Create a Marlowe Contract" marloweContract2
         (assertNoFailedTransactions
         /\ assertNotDone w1 "contract should not have any errors"
-        /\ walletFundsChange w1 (Ada.adaValueOf (-1))
+        -- /\ walletFundsChange w1 (Ada.adaValueOf (-1))
         )
-        (  callEndpoint @"create" w1 (Close) >> handleBlockchainEvents w1 )
+        (  callEndpoint @"create" w1 (Close)
+           >> handleBlockchainEvents w1 )
+    , zeroCouponBondTest
     ]
+
+
+zeroCouponBondTest :: TestTree
+zeroCouponBondTest = checkPredicate @MarloweSchema @ContractError "ZCB" marloweContract2
+    (assertNoFailedTransactions
+    /\ assertNotDone w1 "contract should not have any errors"
+    /\ walletFundsChange alice (adaValueOf (150))
+    ) $ do
+    callEndpoint @"create" w1 Close
+    handleBlockchainEvents w1
+
+    -- Init a contract
+    let alicePk = pubKeyHash $ walletPubKey alice
+        aliceAcc = AccountId 0 alicePk
+        bobPk = pubKeyHash $ walletPubKey bob
+
+    let zeroCouponBond = When [ Case
+            (Deposit aliceAcc alicePk ada (Constant 850_000_000))
+            (Pay aliceAcc (Party bobPk) ada (Constant 850_000_000)
+                (When
+                    [ Case (Deposit aliceAcc bobPk ada (Constant 1000_000_000))
+                        (Pay aliceAcc (Party alicePk) ada (Constant 1000_000_000)
+                        Close)
+                    ] (Slot 200) Close
+                ))] (Slot 100) Close
+
+    callEndpoint @"create" alice zeroCouponBond
+    callEndpoint @"apply-inputs" alice [IDeposit aliceAcc (alicePk) ada 850_000_000]
+    callEndpoint @"apply-inputs" bob [IDeposit aliceAcc ( bobPk) ada 1000_000_000]
+    handleBlockchainEvents alice
+    handleBlockchainEvents bob
+
 
 
 w1, w2 :: Wallet
 w1 = Wallet 1
 w2 = Wallet 2
+
+alice, bob, carol :: Wallet
+alice = Wallet 1
+bob = Wallet 2
+carol = Wallet 3
