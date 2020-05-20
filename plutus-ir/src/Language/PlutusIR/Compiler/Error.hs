aca624531117f2645b0bd655cd47e5e4a3614d59
Nikolaos Bezirgiannis <bezirg@users.noreply.github.com>
2020-05-20 12:14:52 +0200

    Ported PlutusCore.TypeCheck and some PLC.Error ADTs to PIR

--- plutus-ir/src/Language/PlutusIR/Compiler/Error.hs
+++ plutus-ir/src/Language/PlutusIR/Compiler/Error.hs
@@ -5,27 +5,71 @@
 {-# LANGUAGE TemplateHaskell        #-}
 {-# LANGUAGE TypeOperators          #-}
 {-# LANGUAGE UndecidableInstances   #-}
-module Language.PlutusIR.Compiler.Error (Error (..), AsError (..)) where
+module Language.PlutusIR.Compiler.Error
+    ( Error (..)
+    , TypeError (..)
+    , AsTypeError (..)
+    , AsError (..)
+    , PLC.Normalized (..)
+    , PLC.UnknownDynamicBuiltinNameError (..)
+    , throwingEither
+    ) where
 
 import qualified Language.PlutusCore        as PLC
 import qualified Language.PlutusCore.Pretty as PLC
+--import qualified Language.PlutusCore.Error as PLC (throwingEither)
+import qualified Language.PlutusIR as PIR
 
-import           Control.Exception
+import           Control.Exception hiding (TypeError)
 import           Control.Lens
 
 import qualified Data.Text                  as T
 import           Data.Text.Prettyprint.Doc  ((<+>))
 import qualified Data.Text.Prettyprint.Doc  as PP
 import           Data.Typeable
+import GHC.Generics (Generic)
+import Control.Monad.Error.Lens
+import Control.Monad.Except
+
+
+-- | Lifts an 'Either' into an error context where we can embed the 'Left' value into the error.
+-- TODO: literally copy-pasted from PlutusCore.Error to avoid modifying the exposed modules/functions of language-plutus-core package
+throwingEither :: MonadError e m => AReview e t -> Either t a -> m a
+throwingEither r e = case e of
+    Left t  -> throwing r t
+    Right v -> pure v
+
+
+data TypeError uni ann
+    = KindMismatch ann (PLC.Type PLC.TyName uni ()) (PLC.Kind ())  (PLC.Kind ())
+    | TypeMismatch ann
+        (PIR.Term PLC.TyName PLC.Name uni ())
+        (PLC.Type PLC.TyName uni ())
+        (PLC.Normalized (PLC.Type PLC.TyName uni ()))
+    | UnknownDynamicBuiltinName ann PLC.UnknownDynamicBuiltinNameError
+    | InternalTypeErrorE ann (PLC.InternalTypeError uni ann)
+    | FreeTypeVariableE ann PLC.TyName
+    | FreeVariableE ann PLC.Name
+    -- TODO: enable back deriving NFData because PIR.Term does not have it
+    -- TODO: enable back deriving Eq because PIR.Term does not have it, see similar src/Language/PlutusCore/Core/Instance/Eq.hs
+    deriving (Show, Generic) 
+makeClassyPrisms ''TypeError
+
 
 data Error uni a = CompilationError a T.Text -- ^ A generic compilation error.
                  | UnsupportedError a T.Text -- ^ An error relating specifically to an unsupported feature.
                  | PLCError (PLC.Error uni a) -- ^ An error from running some PLC function, lifted into this error type for convenience.
+                 | TypeErrorE (TypeError uni a)
                deriving (Typeable)
 makeClassyPrisms ''Error
 
+
+
 instance PLC.AsTypeError (Error uni a) uni a where
     _TypeError = _PLCError . PLC._TypeError
+
+instance AsTypeError (Error uni a) uni a where
+    _TypeError = _TypeErrorE
 
 instance (PLC.GShow uni, PLC.Closed uni, uni `PLC.Everywhere` PLC.PrettyConst, PP.Pretty a) =>
             Show (Error uni a) where
@@ -37,7 +81,9 @@ instance (PLC.GShow uni, PLC.Closed uni, uni `PLC.Everywhere` PLC.PrettyConst, P
         CompilationError x e -> "Error during compilation:" <+> PP.pretty e <> "(" <> PP.pretty x <> ")"
         UnsupportedError x e -> "Unsupported construct:" <+> PP.pretty e <+> "(" <> PP.pretty x <> ")"
         PLCError e -> PP.vsep [ "Error from the PLC compiler:", PLC.prettyBy config e ]
-
+        -- TODO: add instance Pretty PIR.TypeError and hook it up here
+        TypeErrorE e -> PP.vsep [ "Error from the PIR typechecker:"]
+    
 instance ( PLC.GShow uni, PLC.Closed uni, uni `PLC.Everywhere` PLC.PrettyConst, PP.Pretty a
          , Typeable uni, Typeable a
          ) => Exception (Error uni a)
---
+++ plutus-ir/src/Language/PlutusIR/TypeCheck.hs
@@ -0,0 +1,108 @@
+-- | Kind/type inference/checking.
+
+{-# LANGUAGE TypeOperators #-}
+
+module Language.PlutusIR.TypeCheck
+    (
+    -- * Configuration.
+      DynamicBuiltinNameTypes (..)
+    , TypeCheckConfig (..)
+    , tccDynamicBuiltinNameTypes
+    , defConfig
+    , dynamicBuiltinNameMeaningsToTypes
+    -- * Kind/type inference/checking.
+    , inferKind
+    , checkKind
+    , typeOfBuiltinName
+    , inferType
+    , checkType
+    , inferTypeOfProgram
+    , checkTypeOfProgram
+    ) where
+
+import           Language.PlutusCore.Constant
+import Language.PlutusIR
+import           Language.PlutusIR.Compiler.Error
+import           Language.PlutusIR.Normalize
+import           Language.PlutusCore.Quote
+import           Language.PlutusCore.Rename
+import           Language.PlutusIR.TypeCheck.Internal
+import           Language.PlutusCore.Universe
+
+import           Control.Monad.Except
+
+-- | The default 'TypeCheckConfig'.
+defConfig :: TypeCheckConfig uni
+defConfig = TypeCheckConfig mempty
+
+-- | Extract the 'TypeScheme' from a 'DynamicBuiltinNameMeaning' and convert it to the
+-- corresponding @Type TyName@ for each row of a 'DynamicBuiltinNameMeanings'.
+dynamicBuiltinNameMeaningsToTypes
+    :: (AsTypeError e uni ann, MonadError e m, MonadQuote m)
+    => ann -> DynamicBuiltinNameMeanings uni -> m (DynamicBuiltinNameTypes uni)
+dynamicBuiltinNameMeaningsToTypes ann (DynamicBuiltinNameMeanings means) = do
+    let getType mean = do
+            let ty = dynamicBuiltinNameMeaningToType mean
+            _ <- inferKind defConfig $ ann <$ ty
+            pure <$> normalizeType ty
+    DynamicBuiltinNameTypes <$> traverse getType means
+
+-- | Infer the kind of a type.
+inferKind
+    :: (AsTypeError e uni ann, MonadError e m, MonadQuote m)
+    => TypeCheckConfig uni -> Type TyName uni ann -> m (Kind ())
+inferKind config = runTypeCheckM config . inferKindM
+
+-- | Check a type against a kind.
+-- Infers the kind of the type and checks that it's equal to the given kind
+-- throwing a 'TypeError' (annotated with the value of the @ann@ argument) otherwise.
+checkKind
+    :: (AsTypeError e uni ann, MonadError e m, MonadQuote m)
+    => TypeCheckConfig uni -> ann -> Type TyName uni ann -> Kind () -> m ()
+checkKind config ann ty = runTypeCheckM config . checkKindM ann ty
+
+-- | Infer the type of a term.
+inferType
+    :: ( AsTypeError e uni ann, MonadError e m, MonadQuote m
+       , GShow uni, GEq uni, DefaultUni <: uni
+       )
+    => TypeCheckConfig uni -> Term TyName Name uni ann -> m (Normalized (Type TyName uni ()))
+inferType config = rename >=> runTypeCheckM config . inferTypeM
+
+-- | Check a term against a type.
+-- Infers the type of the term and checks that it's equal to the given type
+-- throwing a 'TypeError' (annotated with the value of the @ann@ argument) otherwise.
+checkType
+    :: ( AsTypeError e uni ann, MonadError e m, MonadQuote m
+       , GShow uni, GEq uni, DefaultUni <: uni
+       )
+    => TypeCheckConfig uni
+    -> ann
+    -> Term TyName Name uni ann
+    -> Normalized (Type TyName uni ())
+    -> m ()
+checkType config ann term ty = do
+    termRen <- rename term
+    runTypeCheckM config $ checkTypeM ann termRen ty
+
+-- | Infer the type of a program.
+inferTypeOfProgram
+    :: ( AsTypeError e uni ann, MonadError e m, MonadQuote m
+       , GShow uni, GEq uni, DefaultUni <: uni
+       )
+    => TypeCheckConfig uni -> Program TyName Name uni ann -> m (Normalized (Type TyName uni ()))
+inferTypeOfProgram config (Program _ term) = inferType config term
+
+-- | Check a program against a type.
+-- Infers the type of the program and checks that it's equal to the given type
+-- throwing a 'TypeError' (annotated with the value of the @ann@ argument) otherwise.
+checkTypeOfProgram
+    :: (AsTypeError e uni ann, MonadError e m, MonadQuote m
+       , GShow uni, GEq uni, DefaultUni <: uni
+       )
+    => TypeCheckConfig uni
+    -> ann
+    -> Program TyName Name uni ann
+    -> Normalized (Type TyName uni ())
+    -> m ()
+checkTypeOfProgram config ann (Program _ term) = checkType config ann term
